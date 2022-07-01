debe925b09a4fb7776dd5354eadfaaea722b93d0
吉井駿一 <syun_syun01@icloud.com>
2022-07-01 18:50:36 +0900

    :sparkles: loginボタンを追加

--- login-app/App.tsx
+++ login-app/App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>ボタンを押してログインしてね。</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonLabel}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +16,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderColor: "lightgray",
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "#00acee",
+    width: 200,
+    height: 40,
+  },
+  buttonLabel: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+    textAlignVertical: "center",
+    lineHeight: 32,
   },
 });
