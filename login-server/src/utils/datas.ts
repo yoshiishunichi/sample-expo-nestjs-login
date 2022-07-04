9c26173f7bdc638918cbe9a32e4de0cc7352c7ea
吉井駿一 <syun_syun01@icloud.com>
2022-07-04 22:15:36 +0900

    :sparkles: APIキーを無効化

--- login-server/src/utils/datas.ts
+++ login-server/src/utils/datas.ts
@@ -1,2 +1,5 @@
-export const appKey = 'gEegMHK4e4J58Ysbp4EW7oXPi';
-export const appSecret = 'BZ2dKHVTsuaOR9wSJOt8sI1j59sYFUnhYen6OFTaEsRfI0Ej64';
+const API_KEY = '';
+const API_SECRET = '';
+
+export const appKey = API_KEY;
+export const appSecret = API_SECRET;
